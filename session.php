0f7373207466b6e61993dce15b14d8a7d9d057d9
Tanner <tannerbouteiller@gmail.com>
2021-11-09 13:46:04 -0600

    created new file to hold session variables

---
+++ session.php
@@ -0,0 +1,11 @@
+<?php
+if (!isset($_SESSION['user_username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+}
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['user_username']);
+    header("location: login.php");
+}
+?>
