93c2f1cb3c9bdd096508640f2f58817f32f592f6
Tanner <tannerbouteiller@gmail.com>
2021-11-12 11:46:12 -0600

    added navigation, breadcrumb, and sorting logic

--- edit.php
+++ edit.php
@@ -38,7 +38,7 @@ if($_POST && isset($_POST['deleteData']) ){
     exit;
 }
 
-//UPDATE
+//UPDATE FUNCTIONALITY
 if($_POST && isset($_POST['recipe_name'])) {
 
     $id = $_SESSION['user_id'];
@@ -94,9 +94,55 @@ if($_POST && isset($_POST['recipe_name'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Edit Recipe</title>
 </head>
 <body>
+<!--MAIN NAV BAR-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Right Recipe</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+        <?php if(isset($_SESSION['user_username'])):?>
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="create.php">Create Recipe</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="myrecipe.php">My Recipes</a>
+          </li>
+          <a class="nav-link" aria-current="page" href="index.php?logout='1'">Sign Out<span class="navbar-text d-inline d-lg-none"> - <b><?php echo $_SESSION['user_displayName']?></b></span></a>
+        <?php else:?>
+          <a class="nav-link dropdown-toggle" href="login.php" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Sign In
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="login.php">Login</a></li>
+            <li><a class="dropdown-item" href="register.php">Register</a></li>
+          </ul>
+        </li>
+      </ul>
+      <?php endif?>
+    </div>
+  </div>
+  <?php if(isset($_SESSION['user_username'])):?>
+      <span class="navbar-text ms-3 pe-4 d-none d-lg-block"><b><?php echo $_SESSION['user_displayName']?></b></span>
+  <?php endif?>
+</nav>
+<!--BREADCRUMB NAV-->
+<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+    <ol class="breadcrumb ps-2 pt-1">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Edit</li>
+    </ol>
+</nav>
 <!--CREATE FORM-->
 <div class="container">
         <form class="needs-validation" id="form" name="submit" action="edit.php" method="POST" novalidate>
@@ -109,11 +155,11 @@ if($_POST && isset($_POST['recipe_name'])) {
             <p class="invalid-feedback">Provide a description for your recipe.</p>
             <label for="form-select" class="form-label">Recipe Category:</label> 
             <select name ="recipe_category" class="form-select" aria-label="recipe category" required>
-            <option selected disabled value="">Select a Category:</option>
-                <option value="1|Breakfast">Breakfast</option>
-                <option value="2|Lunch">Lunch</option>
-                <option value="3|Dinner">Dinner</option>
-                <option value="4|Dessert">Dessert</option>
+            <option disabled value="">Select a Category:</option>
+                <option value="1|Breakfast <?php if($fields['recipe_category'] === 'Breakfast'):?><?='selected="selected"'?><?php endif?>" >Breakfast</option>
+                <option value="2|Lunch" <?php if($fields['recipe_category'] === 'Lunch'):?><?='selected="selected"'?><?php endif?>>Lunch</option>
+                <option value="3|Dinner <?php if($fields['recipe_category'] === 'Dinner'):?><?='selected="selected"'?><?php endif?>">Dinner</option>
+                <option value="4|Dessert <?php if($fields['recipe_category'] === 'Dessert'):?><?='selected="selected"'?><?php endif?>">Dessert</option>
             </select>
             <label for="recipe_ingredients" class="form-label">Ingredients:</label> 
             <textarea class="form-control" id="recipe_ingredients" name="recipe_ingredients" placeholder="Enter your ingredients here..."aria-describedby="recipeIngredientsHelp"><?=$fields['recipe_ingredients']?></textarea>
@@ -129,5 +175,6 @@ if($_POST && isset($_POST['recipe_name'])) {
             </div>
         </form>
     </div>
+<script src="validator.js"></script>  
 </body>
 </html>
